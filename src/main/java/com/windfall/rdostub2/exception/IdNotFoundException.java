f872f70c5e1f7d459719f483052cb35df869c17e
k-m-task <kyle.meezan@gmail.com>
2021-01-05 21:50:04 -0600

    Set up error handling on Json POSTs

---
+++ src/main/java/com/windfall/rdostub2/exception/IdNotFoundException.java
@@ -0,0 +1,22 @@
+package com.windfall.rdostub2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NO_CONTENT, reason = "ID field either missing or malformed")
+public class IdNotFoundException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1933739380349456570L;
+
+    public IdNotFoundException() {
+
+        super("Super Message 204");
+    }
+    public IdNotFoundException(String message) {
+
+        super(message);
+    }
+}
