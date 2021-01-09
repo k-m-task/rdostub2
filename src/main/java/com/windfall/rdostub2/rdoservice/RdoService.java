0ee441219c735c14960858549cd98d6727ca0445
k-m-task <kyle.meezan@gmail.com>
2021-01-08 18:13:08 -0600

    Basic additions to RdoService

--- src/main/java/com/windfall/rdostub2/rdoservice/RdoService.java
+++ src/main/java/com/windfall/rdostub2/rdoservice/RdoService.java
@@ -1,5 +1,10 @@
 package com.windfall.rdostub2.rdoservice;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RdoService {
-    
+    public void say(String s) {
+        System.out.println(s);
+    }
 }
